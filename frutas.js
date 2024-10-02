702b588d138b1ad09465d1ebe145eacdb62f9048
Bipper07 <nettoelyeser@gmail.com>
2024-10-02 20:58:16 -0300

    segundo commit - teste

--- frutas.js
+++ frutas.js
@@ -1,2 +1,5 @@
-const frutas = ["aa", "bb", "cc", "dd"]
+const frutas = ["maçã", "banana", "cereja", "morango", "mirtilo"]
+
 console.log (frutas[0])
+
+console.log("a ultima fruta adicionada foi: " + frutas[4])
---
+++ media.js
@@ -0,0 +1,11 @@
+function media(a, b, c){
+    let result = (a * b * c) / 3  
+    if(result >= 6){
+        console.log("o aluno foi aprovado")
+    } else {
+        console.log("o aluno nao foi aprovado")
+    }
+    
+}
+
+media(2, 4, 9)
